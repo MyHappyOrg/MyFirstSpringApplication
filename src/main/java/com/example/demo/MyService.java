6ad2c0602827ab3f1b82cab3285b432aac7b8272
Happy <thinkgood2all99@gmail.com>
2019-11-28 17:30:41 +0530

    Updated Util mapping

--- src/main/java/com/example/demo/MyService.java
+++ src/main/java/com/example/demo/MyService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyService {
 
-	@GetMapping("/")
+	@GetMapping("/v1/util/health")
 	public String getMessage() {
 		String json = "{status: 'UP',healthInfo: { status: 'UP', appName: 'My First Spring Application' }}";
 		return json;
